432b27bcb28783fdfa925564647fb6970ebc0948
guga7895 <guga7894@gmail.com>
2024-10-28 21:43:33 -0300

    Adicionando mudanças no repositório "Cuidando_do_meu_bairro".

--- features/localhost/step_definitions/check_map_year.rb
+++ features/localhost/step_definitions/check_map_year.rb
@@ -1,12 +1,3 @@
-require 'capybara/rspec'
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    system('npm run serve')
-    sleep 50
-  end
-end
-
 Dado('que estou na página do mapa para verificar o ano') do
     visit '/map-despesas'
   end
@@ -15,7 +6,9 @@ Dado('que estou na página do mapa para verificar o ano') do
     current_year = Time.now.year.to_s    
     expect(page).to have_selector('button[data-dropdown-toggle="dropdownYear"]')
     # esperar 3 segundos, pois as configurações das datas demoram um tempinho para carregar (aparece nil ao carregar).
-    sleep 3
-    year_button = find('button[data-dropdown-toggle="dropdownYear"]')
+    sleep 5
+    year_button = find('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-10.lg\:p-10.pt-10.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div > button')
+    year_button2 = find('#navbar > ul > li:nth-child(6) > div > button')
     expect(year_button.text).to include(current_year)
+    expect(year_button2.text).to include(current_year)
   end
