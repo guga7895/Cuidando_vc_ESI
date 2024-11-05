dc796d393d4520b01bcb3e432bb0ecb1df658d92
guga7895 <guga7894@gmail.com>
2024-11-05 20:50:37 -0300

    Consertando o teste de aceitação do check_map_year

--- testes_cucumber/localhost/step_definitions/check_map_year.rb
+++ testes_cucumber/localhost/step_definitions/check_map_year.rb
@@ -7,7 +7,7 @@ Então('o ano padrão deve ser o ano atual') do
   expect(page).to have_selector('button[data-dropdown-toggle="dropdownYear"]')
   # esperar 3 segundos, pois as configurações das datas demoram um tempinho para carregar (aparece nil ao carregar).
   sleep 5
-  year_button = find('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-10.lg\:p-10.pt-10.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div > button')
+  year_button = find('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-8.lg\:p-8.pt-8.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div > button')
   year_button2 = find('#navbar > ul > li:nth-child(6) > div > button')
   expect(year_button.text).to include(current_year)
   expect(year_button2.text).to include(current_year)
