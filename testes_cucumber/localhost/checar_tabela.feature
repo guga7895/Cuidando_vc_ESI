c7220212a1a7426b0fe5a3c0b4477bf4eb74688e
Felipe Cavalcante Lins <fcavalcante2013.fc@gmail.com>
2024-11-07 21:01:50 -0300

    Adicionando testes de aceitação para o mapa, o cadastro e a tabela de distribuição

---
+++ testes_cucumber/localhost/checar_tabela.feature
@@ -0,0 +1,7 @@
+#language: pt
+
+Funcionalidade: Checar tabela de distribuição de recursos
+
+Cenario: Ver os gastos na tabela 
+Dado que eu estou na tela inicial
+Entao eu deveria ver pelo menos um item na tabela de distribuição
--- testes_cucumber/localhost/step_definitions/map_despesas_steps.rb
+++ testes_cucumber/localhost/step_definitions/map_despesas_steps.rb
@@ -2,6 +2,14 @@ Dado('que estou na página do mapa') do
     visit '/map-despesas'
 end
   
+Quando /eu mudo o ano para 2023/ do
+    visit '2023/1'
+end
+    
+Então('eu deveria ver pelo menos um marcador') do
+    page.should have_css("#map-container > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-marker-pane > div")
+end
+
 Então('devo ver o contêiner do mapa') do
     expect(page).to have_css('.c-map')
 end
